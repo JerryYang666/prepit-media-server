7c12e973287c7e067c2e7163a928bbbfbf1c3710
Ruihuang Yang <rxy216@case.edu>
2024-06-29 22:48:37 -0400

    feat: support feedback processing in API server

--- media_api/main.py
+++ media_api/main.py
@@ -22,19 +22,24 @@ logging.basicConfig(
 
 app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
 
+RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", "rabbitmq")
+RABBITMQ_QUEUE = os.getenv("RABBITMQ_QUEUE", "prepit_processing")
+UNPROCESSED_MEDIA_DIR = "./unprocessed_media"
 
-async def send_to_queue(file_name, metadata_name):
-    connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
+
+async def send_audio_to_queue(file_name, metadata_name):
+    connection = pika.BlockingConnection(pika.ConnectionParameters(RABBITMQ_HOST))
     channel = connection.channel()
 
-    channel.queue_declare(queue='audio_processing', durable=True)
+    channel.queue_declare(queue=RABBITMQ_QUEUE, durable=True)
 
     message = json.dumps({
+        'task_type': 'audio_processing',  # 'audio_processing' or 'feedback_processing'
         'file_name': file_name,
         'metadata_name': metadata_name
     })
     channel.basic_publish(exchange='',
-                          routing_key='audio_processing',
+                          routing_key=RABBITMQ_QUEUE,
                           body=message,
                           properties=pika.BasicProperties(
                               delivery_mode=2,  # make message persistent
@@ -42,7 +47,23 @@ async def send_to_queue(file_name, metadata_name):
     connection.close()
 
 
-UNPROCESSED_MEDIA_DIR = "./unprocessed_media"
+async def send_feedback_to_queue(messages_filename):
+    connection = pika.BlockingConnection(pika.ConnectionParameters(RABBITMQ_HOST))
+    channel = connection.channel()
+
+    channel.queue_declare(queue=RABBITMQ_QUEUE, durable=True)
+
+    message = json.dumps({
+        'task_type': 'feedback_processing',  # 'audio_processing' or 'feedback_processing
+        'messages_filename': messages_filename
+    })
+    channel.basic_publish(exchange='',
+                          routing_key=RABBITMQ_QUEUE,
+                          body=message,
+                          properties=pika.BasicProperties(
+                              delivery_mode=2,  # make message persistent
+                          ))
+    connection.close()
 
 
 async def generate_dynamic_auth_code():
@@ -54,7 +75,7 @@ async def generate_dynamic_auth_code():
 
 
 @app.post("/new_audio_processing_task")
-async def root(
+async def audio_processing_task(
         metadata_file: UploadFile = File(...),
         wav_file: UploadFile = File(...),
         thread_id: str = Form(...),
@@ -76,9 +97,9 @@ async def root(
         with open(wav_file_path, "wb") as f:
             f.write(await wav_file.read())
 
-        await send_to_queue(wav_file.filename, metadata_file.filename)
+        await send_audio_to_queue(wav_file.filename, metadata_file.filename)
 
-        return {"message": f"Processing started for {wav_file.filename}, {metadata_file.filename}",
+        return {"message": f"Audio processing queued for {wav_file.filename}, {metadata_file.filename}",
                 "wav_file_name": wav_file.filename,
                 "metadata_file_name": metadata_file.filename,
                 "thread_id": thread_id,
@@ -88,6 +109,36 @@ async def root(
         return HTTPException(status_code=500, detail="Error processing audio")
 
 
-@app.get("/tttt")
+@app.post("/new_feedback_processing_task")
+async def feedback_processing_task(
+        messages_file: UploadFile = File(...),
+        thread_id: str = Form(...),
+        agent_id: str = Form(...),
+        step_id: int = Form(...),
+        dynamic_auth_token: str = Form(...)
+):
+    # Validate the dynamic auth token
+    expected_dynamic_auth_token = await generate_dynamic_auth_code()
+    if dynamic_auth_token != expected_dynamic_auth_token:
+        return HTTPException(status_code=401, detail="Access Denied")
+    try:
+        # Save metadata file
+        metadata_path = os.path.join(UNPROCESSED_MEDIA_DIR, messages_file.filename)
+        with open(metadata_path, "wb") as f:
+            f.write(await messages_file.read())
+
+        await send_feedback_to_queue(messages_file.filename)
+
+        return {"message": f"Feedback processing queued for {messages_file.filename}",
+                "messages_filename": messages_file.filename,
+                "thread_id": thread_id,
+                "agent_id": agent_id,
+                "step_id": step_id}
+    except Exception as e:
+        logging.error(f"Error processing feedback: {e}")
+        return HTTPException(status_code=500, detail="Error processing feedback")
+
+
+@app.get("/tttt12341234")
 async def root():
     return {"message": "Hello World"}
